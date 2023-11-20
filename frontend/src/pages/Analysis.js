65c7246c7f0b86d2043d2831c6940402fd113a6d
Amoghtech <amogh.1923cs1192@kiet.edu>
2023-11-21 00:00:05 +0530

    changes

--- frontend/src/pages/Analysis.js
+++ frontend/src/pages/Analysis.js
@@ -1,6 +1,5 @@
 import React from "react";
 import PageNameWithDate from "../components/PageNameWithDate";
-
 const Analysis = () => {
   return (
     <div>
--- frontend/src/utils/routes.js
+++ frontend/src/utils/routes.js
@@ -16,3 +16,10 @@ export const routesList = [
     component: <Analysis />,
   },
 ];
+export const getRandomColors = function getRandomLightColor() {
+  const red = Math.floor(Math.random() * 155) + 100; 
+  const green = Math.floor(Math.random() * 155) + 100;
+  const blue = Math.floor(Math.random() * 155) + 100;
+  const color = `#${red.toString(16)}${green.toString(16)}${blue.toString(16)}`;
+  return color;
+};
